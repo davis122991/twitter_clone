e889744988afd820ce2292274642573a5a5715e9
davis122991 <110353699+davis122991@users.noreply.github.com>
2024-05-24 21:44:09 +0300

    Completed auth & protected routes

--- frontend/src/pages/auth/signup/LoginPage.jsx
+++ frontend/src/pages/auth/signup/LoginPage.jsx
@@ -6,22 +6,56 @@ import XSvg from '../../../components/svgs/X';
 import { MdOutlineMail } from 'react-icons/md';
 import { MdPassword } from 'react-icons/md';
 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+
 const LoginPage = () => {
   const [formData, setFormData] = useState({
     username: '',
     password: '',
   });
 
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: loginMutation,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: async ({ username, password }) => {
+      try {
+        const res = await fetch('/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.error || 'Something went wrong');
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['authUser'] });
+    },
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+
+    loginMutation(formData);
   };
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
-  const isError = false;
 
   return (
     <div className='max-w-screen-xl mx-auto flex h-screen'>
@@ -67,11 +101,11 @@ const LoginPage = () => {
 
           {/* Login button */}
           <button className='btn rounded-full btn-primary text-white'>
-            Login
+            {isPending ? 'Loading...' : 'Login'}
           </button>
 
           {/* Error container */}
-          {isError && <p className='text-red-500'>Something went wrong</p>}
+          {isError && <p className='text-red-500'>{error.message}</p>}
         </form>
 
         {/* Or container */}
