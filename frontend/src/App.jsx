380f62b2c3d72913327fa949f06b7c79c7dd325c
davis122991 <110353699+davis122991@users.noreply.github.com>
2024-05-24 21:48:51 +0300

    Fixed logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,12 +19,13 @@ function App() {
     queryFn: async () => {
       try {
         const res = await fetch('/api/auth/me');
-        const data = res.json();
+        const data = await res.json();
         if (data.error) return null;
-        if (!res.ok) throw new Error(data.error || 'Something went wrong');
+        if (!res.ok) {
+          throw new Error(data.error || 'Something went wrong');
+        }
 
         console.log('authUser is here:', data);
-
         return data;
       } catch (error) {
         throw new Error(error);
