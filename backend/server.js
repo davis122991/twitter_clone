553764082c99aac06ae0749ef83acf47d069b635
davis122991 <110353699+davis122991@users.noreply.github.com>
2024-06-03 17:09:34 +0300

    finishing touches

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import notificationRoutes from './routes/notification.route.js';
 
-import connectMongoDB from './db/connectMongoDb.js';
+import connectMongoDB from './db/connectMongoDB.js';
 
 dotenv.config();
 
@@ -20,12 +20,12 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
 
@@ -35,7 +35,7 @@ app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
